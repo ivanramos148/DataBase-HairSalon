ba3cfa7a0eeaa8be22b3a7fc6e086ed1e1bce952
ivan ramos <ivanramos@gmail.com>
2018-03-01 10:19:59 -0800

    add Controllers and Clients.cs to it

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System;
+
+namespace HairSalon.Controllers
+{
+    public class ClientsController : Controller
+    {
+      [HttpGet("/clients")]
+      public ActionResult Index()
+      {
+        List<Client> allClients = Client.GetAll();
+        return View(allClients);
+      }
+
+
+      [HttpGet("/stylists/{id}/clients/new")]
+      public ActionResult CreateForm(int id)
+      {
+        Stylist thisStylist = Stylist.Find(id);
+        string thisName = thisStylist.GetStylistName();
+        int thisId = thisStylist.GetId();
+        return View(thisStylist);
+      }
+
+      [HttpPost("/stylists/{id}/clients/new")]
+      public ActionResult Create(int id)
+      {
+        Stylist thisStylist = Stylist.Find(id);
+        string thisName = thisStylist.GetStylistName();
+        int thisId = thisStylist.GetId();
+
+
+        string newClientName = Request.Form["new-client-name"];
+        int newStylistId = thisStylist.GetId();
+        Client newClient = new Client(newClientName, newStylistId);
+        newClient.Save();
+
+        return View("~/Views/Stylists/Profile.cshtml", thisStylist);
+      }
+
+      [HttpPost("/clients/{id}/delete")]
+      public ActionResult Delete(int id)
+      {
+        Client thisClient = Client.Find(id);
+        Stylist thisStylist = thisClient.FindStylist(thisClient.GetStylistId());
+        thisClient.DeleteClient();
+
+        return View(thisStylist);
+      }
+
+    }
+}
---
+++ HairSalon/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System;
+
+namespace HairSalon.Controllers
+{
+    public class HomeController : Controller
+    {
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View(allStylists);
+      }
+      
+    }
+}
